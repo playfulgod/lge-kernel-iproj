0aa5df505f02f7773416709e16f4bfd100e9ae8e
Ricardo Cerqueira <github@cerqueira.org>
2012-03-20 11:33:00 +0000

    scm: Fix building with newer binutils

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -174,7 +174,9 @@ static u32 smc(u32 cmd_addr)
 	register u32 r2 asm("r2") = cmd_addr;
 	do {
 		asm volatile(
+#ifdef PLUSSEC
                         ".arch_extension sec\n"
+#endif
 			__asmeq("%0", "r0")
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
@@ -300,6 +302,9 @@ u32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
+#ifdef PLUSSEC
+                ".arch_extension sec\n"
+#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2)
@@ -332,6 +337,9 @@ u32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
+#ifdef PLUSSEC
+                ".arch_extension sec\n"
+#endif
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3));
@@ -355,11 +363,13 @@ u32 scm_get_version(void)
 	r1 = (u32)&context_id;
 	do {
 		asm volatile(
-                        ".arch_extension sec\n"
 			__asmeq("%0", "r0")
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
+#ifdef PLUSSEC
+			".arch_extension sec\n"
+#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0), "=r" (r1)
 			: "r" (r0), "r" (r1)
